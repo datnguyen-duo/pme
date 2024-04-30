d1c450d855913e710ab2fa400f88e05e8b6f63c3
Nemanja <nemanjapavloviic@gmail.com>
2024-04-30 11:41:45 +0200

    dynamic variable for header z index added and fixed map section z-index where maps small popup is overlaying over the header

--- build/main.asset.php
+++ build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '46613bf8088a9e1aaa13');
+<?php return array('dependencies' => array('jquery'), 'version' => '38e48c384fdac0cb2654');
